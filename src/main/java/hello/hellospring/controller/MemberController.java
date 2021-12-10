bdee584fb6a7afd72c62bd865a3c16b7d0f7f4a8
SeungkeyMouse <ksg19980@gmail.com>
2021-12-10 22:42:20 +0900

    회원 관리 예제 - 웹 MVC 개발 회원 웹 기능 - 조회

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -31,5 +34,13 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";//회원가입이 끝나니까 홈화면으로 돌려보냄.
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();//멤버 다 가져오기
+        model.addAttribute("members", members);//멤버리스트를 모델에 담아
+        return "members/memberList";
+
     }
 }
